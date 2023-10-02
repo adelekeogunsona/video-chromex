8e083a7aec3f99fa0340c046c64282e05d8068aa
Adeleke Ogunsona <ogunsonaadedotun@gmail.com>
2023-10-02 11:07:28 +0100

    Revert "Revert "Added Swagger Documentation""
    This reverts commit a9f4b62240bfc3333327416b7a9e5d774f2a5d53.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,18 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Server(
+ *     url=L5_SWAGGER_CONST_HOST,
+ *     description="API Server"
+ * ),
+ * @OA\Info(
+ *     title="API DOCUMENTATION",
+ *     version="0.1",
+ *     description="This is the API documentation for the Video Streaming App.",
+ * ),
+ * @OA\PathItem(path="/"),
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
