8eb5b99b5ec994e6cae7b7f5cf5ed67a3da496ff
Adeleke Ogunsona <ogunsonaadedotun@gmail.com>
2023-10-02 11:22:17 +0100

    Added OpenAPI annotations

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -13,7 +13,59 @@ use Symfony\Component\HttpFoundation\Response;
 class VideoController extends Controller
 {
     /**
-     * Display all videos.
+     * Retrieve all videos.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     *
+     * @OA\Get(
+     *     path="/videos",
+     *     summary="Retrieve all videos",
+     *     operationId="getVideos",
+     *     tags={"Endpoints"},
+     *     @OA\Response(
+     *         response=200,
+     *         description="Videos retrieved successfully",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="Videos retrieved successfully."
+     *             ),
+     *             @OA\Property(
+     *                 property="data",
+     *                 type="array",
+     *                 @OA\Items(
+     *                     @OA\Property(
+     *                         property="id",
+     *                         type="integer",
+     *                         example="1"
+     *                     ),
+     *                     @OA\Property(
+     *                         property="title",
+     *                         type="string",
+     *                         example="Video-1"
+     *                     ),
+     *                     @OA\Property(
+     *                         property="public_url",
+     *                         type="string",
+     *                         example="http://example.com/storage/videos/87uiytbHGf-1/video.webm"
+     *                     )
+     *                 )
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="No videos found",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="No videos found."
+     *             )
+     *         )
+     *     )
+     * )
      */
     public function index()
     {
@@ -32,7 +84,68 @@ class VideoController extends Controller
     }
 
     /**
-     * Display the specified video.
+     * Retrieve a video.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
+     *
+     * @OA\Get(
+     *     path="/video/{id}",
+     *     summary="Retrieve a video",
+     *     operationId="getVideo",
+     *     tags={"Endpoints"},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="Video ID",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="integer",
+     *             example="1"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Video retrieved successfully",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="Video retrieved successfully."
+     *             ),
+     *             @OA\Property(
+     *                 property="data",
+     *                 type="object",
+     *                 @OA\Property(
+     *                     property="id",
+     *                     type="integer",
+     *                     example="1"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="title",
+     *                     type="string",
+     *                     example="Video-1"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="public_url",
+     *                     type="string",
+     *                     example="http://example.com/storage/videos/87uiytbHGf-1/video.webm"
+     *                 )
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Video not found",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="Video not found."
+     *             )
+     *         )
+     *     )
+     * )
      */
     public function show($id)
     {
@@ -50,7 +163,56 @@ class VideoController extends Controller
     }
 
     /**
-     * Start streaming a new video.
+     * Start streaming a video.
+     *
+     * @param \App\Http\Requests\StoreVideoRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     *
+     * @OA\Post(
+     *     path="/video/stream",
+     *     summary="Stream a video",
+     *     operationId="streamVideo",
+     *     tags={"Endpoints"},
+     *     @OA\RequestBody(
+     *         description="Video details",
+     *         required=true,
+     *         @OA\MediaType(
+     *             mediaType="multipart/form-data",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                     property="title",
+     *                     type="string",
+     *                 ),
+     *                 @OA\Property(
+     *                     property="blob",
+     *                     type="file",
+     *                 )
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Video streamed successfully",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="Video streamed successfully."
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Video already completed",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="Video already completed."
+     *             )
+     *         )
+     *     )
+     * )
      */
     public function stream(StoreVideoRequest $request)
     {
@@ -70,23 +232,99 @@ class VideoController extends Controller
         }
 
         return response()->json([
-            'message' => 'Video streamed successfully.',
+            'message' => 'Video streamed successfully.'
         ], Response::HTTP_OK);
     }
 
     /**
-     * Mark video as completed.
+     * Stop streaming a video.
+     *
+     * @param int $title
+     * @return \Illuminate\Http\JsonResponse
+     *
+     * @OA\Get(
+     *     path="/video/end-stream/{title}",
+     *     summary="Stop streaming a video",
+     *     operationId="stopVideo",
+     *     tags={"Endpoints"},
+     *     @OA\Parameter(
+     *         name="title",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string",
+     *             example="Video-1"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Video completed successfully",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="Video completed successfully."
+     *             ),
+     *             @OA\Property(
+     *                 property="data",
+     *                 type="object",
+     *                 @OA\Property(
+     *                     property="id",
+     *                     type="integer",
+     *                     example="1"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="title",
+     *                     type="string",
+     *                     example="Video-1"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="url",
+     *                     type="string",
+     *                     example="http://example.com/storage/videos/87uiytbHGf.webm"
+     *                 ),
+     *                @OA\Property(
+     *                    property="transcript",
+     *                   type="string",
+     *                  example="This is the transcript of the video."
+     *                )
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Video already completed",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="Video already completed."
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Video not found",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="Video not found."
+     *             )
+     *         )
+     *     )
+     * )
      */
-    public function stop($id)
+    public function stop($title)
     {
         // check if video exists
-        if (!Video::where('id', $id)->exists()) {
+        if (!Video::where('title', $title)->exists()) {
             return response()->json([
                 'message' => 'Video not found.',
             ], Response::HTTP_NOT_FOUND);
         }
 
-        $video = Video::where('id', $id)->first();
+        $video = Video::where('title', $title)->first();
 
         // check if video is already completed
         if ($video->path !== null) {
@@ -102,7 +340,8 @@ class VideoController extends Controller
 
         // load ffmpeg binaries path
         $ffmpeg = FFMpeg::create([
-            'ffmpeg.binaries'  => 'C:/FFmpeg/bin/ffmpeg.exe',   // changing this later
+            'ffmpeg.binaries' => 'C:/FFmpeg/bin/ffmpeg.exe',
+            // changing this later
             'ffprobe.binaries' => 'C:/FFmpeg/bin/ffprobe.exe'
         ]);
 
@@ -110,10 +349,15 @@ class VideoController extends Controller
         $video->save(new WebM(), Storage::path($video_webm_path));
 
         // update the video record
-        Video::where('id', $id)->update([
+        Video::where('title', $title)->update([
             'path' => $video_webm_path,
             'public_url' => $video_webm_url,
         ]);
+
+        return response()->json([
+            'message' => 'Video completed successfully.',
+            'data' => new VideoResource($video),
+        ], Response::HTTP_OK);
     }
 
     /**
